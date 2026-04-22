b1eede97c924dbb73a5042794c2e499258f2b062
Chinmay S Pai <65858550+cpai-07@users.noreply.github.com>
2026-04-22 17:50:27 +0530

    created variable to store all the sum

--- prg1.js
+++ prg1.js
@@ -1,11 +1,11 @@
 function fn(){
-
+    let sum = 0;
     for(let i=1;i<1000;i++){
         if(i%3===0 || i%5===0){
-           console.log(i)
+           sum+=i;
         }
     }
-    
+    console.log(sum)
 }
 
 fn();
